873983f21c34f8f03efc32993e9e4289e89c11ee
Silent <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-24 09:54:45 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import time
 import requests
 
+count = 0
 url = "https://discord.com/api/v9/channels/857466994126618645/typing"
-
 headers = {
-  'authorization': 'ODUwNTg4ODUzMzc4NDgyMTg2.YNP1Jg.xA_tn5vc5eTLqLd-9_x98eRVXc8'
+  'authorization': ''
 }
 
-response = requests.request("POST", url, headers=headers)
-
-print(response.text)
+while True:
+	response = requests.request("POST", url, headers=headers)
+	if response.status_code != 204:
+		print(response.text)
+		print("Failed")
+	else:
+		count += 1
+		print(f"Sent - {count}")
