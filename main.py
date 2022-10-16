46c59725a72fcedb84dda9847dac41329b4b3c51
rosidae <windows10vmstuff@gmail.com>
2022-10-16 04:30:17 -0500

    check if token file exists instead of checking if token file has "none"

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from time import sleep
 import requests
+from os.path import exists
 
-token_file = open("token", "r+")
-if(token_file.read() == "none"):
-	token_file.seek(0)
-	token_file.truncate()
-	token_file.write(input("Enter your token: "))
-token_file.seek(0)
-token = token_file.read()
+if(not exists("token")) :
+	new_token_file = open("token", "w+")
+	new_token_file.write(input("Enter your token: "))
+	new_token_file.seek(0)
+	token = new_token_file.read()
+else:
+	token_file = open("token", "r")
+	token = token_file.read()
 
 channel_id = input("Enter desired channel id: ")
 
